05f99746d188403b9bd7bc63e44f9ad12b1894f6
erin petra sofiya moon <erin@hecke.rs>
2022-10-26 14:51:06 -0500

    clang-format

--- asm/lexer.cpp
+++ asm/lexer.cpp
@@ -4,120 +4,125 @@
 #include <iostream>
 
 Token Lexer::next() noexcept {
-	while (true) {
-		switch (peek()) {
-			default:
-				if (isalpha(peek()) || peek() == '.')
-					return lexIdentifierOrKeyword(cursor);
+  while (true) {
+    switch (peek()) {
+    default:
+      if (isalpha(peek()) || peek() == '.')
+        return lexIdentifierOrKeyword(cursor);
 
-				return tokAtom(Token::Kind::UNEXPECTED);
+      return tokAtom(Token::Kind::UNEXPECTED);
 
-			case '\0':
-				return tokAtom(Token::Kind::ENDOFFILE);
+    case '\0':
+      return tokAtom(Token::Kind::ENDOFFILE);
 
-			case ' ':
-			case '\t':
-			case '\n':
-			case '\r':
-				// skip whitespace
-				eat();
-				continue;
+    case ' ':
+    case '\t':
+    case '\n':
+    case '\r':
+      // skip whitespace
+      eat();
+      continue;
 
-			case ';':
-				eatComment();
-				continue;
+    case ';':
+      eatComment();
+      continue;
 
-			case ':': return tokAtom(Token::Kind::COLON);
-			case ',': return tokAtom(Token::Kind::COMMA);
-			case '[': return tokAtom(Token::Kind::L_SQUARE);
-			case '{': return tokAtom(Token::Kind::L_BRACE);
-			case ']': return tokAtom(Token::Kind::R_SQUARE);
-			case '}': return tokAtom(Token::Kind::R_BRACE);
-			case '#': return tokAtom(Token::Kind::HASH);
-			case '+':
-				if (peek(+1) == '=') {
-					// `+=`
-					const char* start = cursor;
-					eat(2);
-					return tokFrom(start, Token::Kind::PLUSEQUAL);
-				}
-				return tokAtom(Token::Kind::PLUS);
+    case ':':
+      return tokAtom(Token::Kind::COLON);
+    case ',':
+      return tokAtom(Token::Kind::COMMA);
+    case '[':
+      return tokAtom(Token::Kind::L_SQUARE);
+    case '{':
+      return tokAtom(Token::Kind::L_BRACE);
+    case ']':
+      return tokAtom(Token::Kind::R_SQUARE);
+    case '}':
+      return tokAtom(Token::Kind::R_BRACE);
+    case '#':
+      return tokAtom(Token::Kind::HASH);
+    case '+':
+      if (peek(+1) == '=') {
+        // `+=`
+        const char *start = cursor;
+        eat(2);
+        return tokFrom(start, Token::Kind::PLUSEQUAL);
+      }
+      return tokAtom(Token::Kind::PLUS);
 
-
-			case '0':
-			case '1':
-			case '2':
-			case '3':
-			case '4':
-			case '5':
-			case '6':
-			case '7':
-			case '8':
-			case '9':
-				return lexNumber(cursor);
-		}
-	}
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      return lexNumber(cursor);
+    }
+  }
 }
 
 bool isidchar(char x) {
-	return isalpha(x) || isdigit(x)
-		|| x == '_' || x == '$' || x == '.';
+  return isalpha(x) || isdigit(x) || x == '_' || x == '$' || x == '.';
 }
 
-Token Lexer::lexIdentifierOrKeyword(const char* tok_start) {
-	while (isidchar(peek())) {
-		eat();
-	}
+Token Lexer::lexIdentifierOrKeyword(const char *tok_start) {
+  while (isidchar(peek())) {
+    eat();
+  }
 
-	return tokFrom(tok_start, Token::Kind::IDENTIFIER);
+  return tokFrom(tok_start, Token::Kind::IDENTIFIER);
 }
 
-Token Lexer::lexNumber(const char* tok_start) {
-	assert(isdigit(peek()));
+Token Lexer::lexNumber(const char *tok_start) {
+  assert(isdigit(peek()));
 
-	// hex!
-	if (peek() == '0' && peek(+1) == 'x') {
-		cursor += 2;
+  // hex!
+  if (peek() == '0' && peek(+1) == 'x') {
+    cursor += 2;
 
-		while (isxdigit(peek()))
-			eat();
+    while (isxdigit(peek()))
+      eat();
 
-		return tokFrom(tok_start, Token::Kind::INTEGER);
-	}
+    return tokFrom(tok_start, Token::Kind::INTEGER);
+  }
 
-	// normal case: decimals
-	while (isdigit(peek()))
-		eat();
+  // normal case: decimals
+  while (isdigit(peek()))
+    eat();
 
-	// TODO: float literals! exponentials!
+  // TODO: float literals! exponentials!
 
-	return tokFrom(tok_start, Token::Kind::INTEGER);
+  return tokFrom(tok_start, Token::Kind::INTEGER);
 }
 
 void Lexer::eatComment() {
-	while (true) {
-		switch (eat()) {
-		case '\n':
-		case '\r':
-			// end of comment!
-			return;
+  while (true) {
+    switch (eat()) {
+    case '\n':
+    case '\r':
+      // end of comment!
+      return;
 
-		case '\0':
-			// rewind so we can emit the EOF token on \0
-			cursor--;
-			return;
+    case '\0':
+      // rewind so we can emit the EOF token on \0
+      cursor--;
+      return;
 
-		default:
-			break;
-		}
-	}
+    default:
+      break;
+    }
+  }
 }
 
-std::ostream& operator<<(std::ostream& os, const Token::Kind& kind) {
-    static const std::string names[] = {
-        #define TOKEN(ident) #ident
-        #include "tokens.def"
-    };
+std::ostream &operator<<(std::ostream &os, const Token::Kind &kind) {
+  static const std::string names[] = {
+#define TOKEN(ident) #ident
+#include "tokens.def"
+  };
 
-    return os << names[static_cast<int>(kind)];
+  return os << names[static_cast<int>(kind)];
 }
--- asm/main.cpp
+++ asm/main.cpp
@@ -1,48 +1,45 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
 #include <argparse/argparse.hpp>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
 #include "lexer.h"
 
-int main(int argc, char* argv[]) {
-    argparse::ArgumentParser ap("asm");
+int main(int argc, char *argv[]) {
+  argparse::ArgumentParser ap("asm");
 
-    ap.add_argument("-o", "--output")
-        .help("output path")
-        .default_value(std::string{"out.o"});
+  ap.add_argument("-o", "--output")
+      .help("output path")
+      .default_value(std::string{"out.o"});
 
-    ap.add_argument("sources").nargs(argparse::nargs_pattern::at_least_one);
+  ap.add_argument("sources").nargs(argparse::nargs_pattern::at_least_one);
 
-    try {
-        ap.parse_args(argc, argv);
-    } catch (const std::runtime_error& err) {
-        std::cerr << err.what() << std::endl;
-        std::cerr << ap;
-        std::exit(1);
-    }
+  try {
+    ap.parse_args(argc, argv);
+  } catch (const std::runtime_error &err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << ap;
+    std::exit(1);
+  }
 
+  auto inputs = ap.get<std::vector<std::string>>("sources");
 
-    auto inputs = ap.get<std::vector<std::string>>("sources");
+  std::ifstream f;
+  f.open(inputs[0]);
+  if (!f.is_open()) {
+    std::cerr << "[!] cant open " << inputs[0] << std::endl;
+  }
+  f.seekg(0, std::ios::end);
+  size_t fsize = f.tellg();
+  std::string sourcebuf(fsize, '\0');
+  f.seekg(0);
+  f.read(&sourcebuf[0], fsize);
 
-    std::ifstream f;
-    f.open(inputs[0]);
-    if (!f.is_open()) {
-        std::cerr << "[!] cant open " << inputs[0] << std::endl;
-    }
-    f.seekg(0, std::ios::end);
-    size_t fsize = f.tellg();
-    std::string sourcebuf(fsize, '\0');
-    f.seekg(0);
-    f.read(&sourcebuf[0], fsize);
+  Lexer lexer(sourcebuf.c_str());
 
-    Lexer lexer(sourcebuf.c_str());
-
-    for (auto token = lexer.next();
-         !token.is(Token::Kind::ENDOFFILE);
-         token = lexer.next())
-    {
-        std::cout << token.getKind() << ": '" << token.getLexeme() << "'\n";
-    }
+  for (auto token = lexer.next(); !token.is(Token::Kind::ENDOFFILE);
+       token = lexer.next()) {
+    std::cout << token.getKind() << ": '" << token.getLexeme() << "'\n";
+  }
 }
