f01066baaed7441cd00403ef4573fee01362bafe
Ndubuisi Osuji <osuji@wisc.edu>
2022-11-22 18:32:33 -0600

    Done some

--- sim/instructions.h
+++ sim/instructions.h
@@ -21,13 +21,83 @@ void lih(CPUState& cpu, MemSystem& mem, reg_idx rD, s<18> imm) {
     cpu.r[rD].inner = (cpu.r[rD].inner & ~mask) | (imm.inner << 18);
 }
 
-// -- scalar alu instructions
-void addi(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rS, s<12> imm) {
+/************************************************************************/
+/************************-- scalar alu instruction **********************/
+/************************************************************************/
+
+// ADDI
+void addi(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rS, s<15> imm) {
     cpu.r[rD] = (cpu.r[rS].asSigned() + imm).asUnsigned();
 }
 
+// SUBI
+void subi(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rS, s<15> imm) {
+    cpu.r[rD] = (cpu.r[rS].asSigned() - imm).asUnsigned();
+}
+
+// ANDI
+void andi(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rS, s<15> imm) {
+    cpu.r[rD] = (cpu.r[rS].asSigned() & imm).asUnsigned();
+}
+
+// ORI
+void ori(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rS, s<15> imm) {
+    cpu.r[rD] = (cpu.r[rS].asSigned() | imm).asUnsigned();
+}
+
+// XORI
+void xori(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rS, s<15> imm) {
+    cpu.r[rD] = (cpu.r[rS].asSigned() ^ imm).asUnsigned();
+}
+
+// SHLI
+void shli(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rS, s<15> imm) {
+    cpu.r[rD] = (cpu.r[rS].asSigned() >> imm).asUnsigned();
+}
+
+// SHRI
+void shri(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rS, s<15> imm) {
+    cpu.r[rD] = (cpu.r[rS].asSigned() << imm).asUnsigned();
+}
+
+// ADD
 void add(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rA, reg_idx rB) {
     cpu.r[rD] = (cpu.r[rA].asSigned() + cpu.r[rB].asSigned()).asUnsigned();
+}
+
+// SUB
+void sub(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rA, reg_idx rB) {
+    cpu.r[rD] = (cpu.r[rA].asSigned() + cpu.r[rB].asSigned()).asUnsigned();
+}
+
+// MULT
+void mult(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rA, reg_idx rB) {
+    cpu.r[rD] = (cpu.r[rA].asSigned() * cpu.r[rB].asSigned()).asUnsigned();
+}
+
+// AND
+void and(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rA, reg_idx rB) {
+    cpu.r[rD] = (cpu.r[rA].asSigned() & cpu.r[rB].asSigned()).asUnsigned();
+}
+
+// OR
+void or(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rA, reg_idx rB) {
+    cpu.r[rD] = (cpu.r[rA].asSigned() | cpu.r[rB].asSigned()).asUnsigned();
+}
+
+// XOR
+void xor(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rA, reg_idx rB) {
+    cpu.r[rD] = (cpu.r[rA].asSigned() ^ cpu.r[rB].asSigned()).asUnsigned();
+}
+
+// SHL
+void shl(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rA, reg_idx rB) {
+    cpu.r[rD] = (cpu.r[rA].asSigned() >> cpu.r[rB].asSigned()).asUnsigned();
+}
+
+// SHR
+void shr(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rA, reg_idx rB) {
+    cpu.r[rD] = (cpu.r[rA].asSigned() << cpu.r[rB].asSigned()).asUnsigned();
 }
 
 void cmp(CPUState& cpu, MemSystem& mem, reg_idx rD, reg_idx rA, reg_idx rB) {
@@ -99,10 +169,111 @@ void jalr(CPUState& cpu, MemSystem& mem, reg_idx rT, s<20> imm) {
     cpu.pc.setNextPC(cpu.r[rT].inner + imm._sgn_inner() * 4); // and jump
 }
 
-// -- conditional branches
-void bnzr(CPUState& cpu, MemSystem& mem, reg_idx rT, s<17> imm) {
+/************************************************************************/
+/*******************************-- JUMPS ********************************/
+/************************************************************************/
+
+// JMPR
+void jmpr(CPUState& cpu, MemSystem& mem, reg_idx rT, s<20> imm) {
+    cpu.pc.redirect(cpu.r[rT] + (imm << 2)); // and jump (imm * 4)
+}
+
+// JALR
+void jalr(CPUState& cpu, MemSystem& mem, reg_idx rT, s<20> imm) {
+    cpu.r[31] = cpu.pc.get();         // link
+    cpu.pc.redirect(cpu.r[rT] + (imm << 2)); // and jump (imm * 4)
+}
+
+// JMP
+void jmp(CPUState& cpu, MemSystem& mem, reg_idx rT, s<25> imm) {
+    addr_t nxt_pc = cpu.pc.get() + 4;
+    cpu.pc.redirect(nxt_pc + (imm << 2)); // imm * 4
+}
+
+// JAL
+void jal(CPUState& cpu, MemSystem& mem, reg_idx rT, s<25> imm) {
+    addr_t curr_pc = cpu.pc.get();
+    addr_t nxt_pc = curr_pc + 4;
+    cpu.r[31] = curr_pc;                // link
+    cpu.pc.redirect(nxt_pc + (imm << 2)); // and jump (imm * 4)
+}
+
+/************************************************************************/
+/************************-- Conditional Branches ************************/
+/************************************************************************/
+
+// BNZR
+void bnzr(CPUState& cpu, MemSystem& mem, reg_idx rT, s<15> imm) {
     if (!cpu.f.zero)
         cpu.pc.setNextPC(cpu.r[rT].inner + imm._sgn_inner());
+}
+
+// BEZR
+void bezr(CPUState& cpu, MemSystem& mem, reg_idx rT, s<15> imm) {
+    if (cpu.f.zero)
+        cpu.pc.redirect(cpu.r[rT] + imm);
+}
+
+// BLZR
+void blzr(CPUState& cpu, MemSystem& mem, reg_idx rT, s<15> imm) {
+    if (cpu.f.sign)
+        cpu.pc.redirect(cpu.r[rT] + imm);
+}
+
+// BGZR
+void bgzr(CPUState& cpu, MemSystem& mem, reg_idx rT, s<15> imm) {
+    if (!cpu.f.sign)
+        cpu.pc.redirect(cpu.r[rT] + imm);
+}
+
+// BLER
+void bler(CPUState& cpu, MemSystem& mem, reg_idx rT, s<15> imm) {
+    if (cpu.f.sign | cpu.f.zero)
+        cpu.pc.redirect(cpu.r[rT] + imm);
+}
+
+// BGER
+void bger(CPUState& cpu, MemSystem& mem, reg_idx rT, s<15> imm) {
+    if (!cpu.f.sign | cpu.f.zero)
+        cpu.pc.redirect(cpu.r[rT] + imm);
+}
+
+/** Relative to current PC */
+
+// BNZI
+void bnzi(CPUState& cpu, MemSystem& mem, reg_idx rT, s<15> imm) {
+    if (!cpu.f.zero)
+        cpu.pc.redirect(cpu.pc.get() + imm);
+}
+
+// BEZI
+void bezr(CPUState& cpu, MemSystem& mem, reg_idx rT, s<15> imm) {
+    if (cpu.f.zero)
+        cpu.pc.redirect(cpu.pc.get() + imm);
+}
+
+// BLZI
+void blzr(CPUState& cpu, MemSystem& mem, reg_idx rT, s<15> imm) {
+    if (cpu.f.sign)
+        cpu.pc.redirect(cpu.pc.get() + imm);
+}
+
+// BGZI
+void bgzr(CPUState& cpu, MemSystem& mem, reg_idx rT, s<15> imm) {
+    if (!cpu.f.sign)
+        cpu.pc.redirect(cpu.pc.get() + imm);
+}
+
+// BLEI
+void bler(CPUState& cpu, MemSystem& mem, reg_idx rT, s<15> imm) {
+    if (cpu.f.sign | cpu.f.zero)
+        cpu.pc.redirect(cpu.pc.get() + imm);
+}
+
+// BGEI
+void bgei(CPUState& cpu, MemSystem& mem, reg_idx rT, s<15> imm) {
+    if (!cpu.f.sign | cpu.f.zero)
+        cpu.pc.redirect(cpu.pc.get() + imm);
 }
 
 // -- specials: cache control
