44a8e75611b60af9cafc5db02293080f38ccc9d5
Allison Nicole Seigler <aseigler@wisc.edu>
2022-11-26 16:04:30 -0600

    added load/store instructions also it compiles now

--- libmorph/morph/encoder.h
+++ libmorph/morph/encoder.h
@@ -11,7 +11,8 @@ enum class ScalarArithmeticOp { Add, Sub, Mult, And, Or, Xor, Shr, Shl, Not };
 enum class FloatArithmeticOp { Fadd, Fsub, Fmult, Fdiv};
 enum class VectorArithmeticOp { Vadd, Vsub, Vmult, Vdiv, Vdot, Vdota, Vindx, Vreduce,
     Vsplat, Vswizzle, Vsadd, Vsmult, Vssub, Vsdiv, Vsma, Vmax, Vmin, Vcompsel};
-enum class matrixMultiplyOp {writeA, writeB, writeC, matmul, readC, systolicstep};
+enum class MatrixMultiplyOp { WriteA, WriteB, WriteC, Matmul, ReadC, Systolicstep};
+enum class LoadStoreOp { Lih, Lil, Ld32, Ld36, St32, St36, Vldi, Vsti, Vldr, Vstr};
 }; // namespace isa
 
 class Emitter {
@@ -28,9 +29,11 @@ class Emitter {
     void floatArithmetic(isa::FloatArithmeticOp op, reg_idx rD, reg_idx rA,
                           reg_idx rB);
     void vectorArithmetic(isa::VectorArithmeticOp op, vreg_idx vD, vreg_idx vA,
-                          vreg_idx vB, reg_idx rD, reg_idx rA, u<4> mask);
-    void matrixMultiply(isa::matmulOp op, vreg_idx vD, vreg_idx vA, vreg_idx vB,
-                          u<3> idx, bool high) 
+                          vreg_idx vB, reg_idx rD, reg_idx rA, reg_idx rB, u<4> mask, s<8> imm);
+    void matrixMultiply(isa::MatrixMultiplyOp op, vreg_idx vD, vreg_idx vA, vreg_idx vB,
+                          u<3> idx, bool high);
+    void loadStore(isa::LoadStoreOp op, vreg_idx vD, vreg_idx vA, reg_idx rD, reg_idx rA,
+                        reg_idx rB, u<18> imm, u<4> mask);
                                  
 
     auto getData() -> const auto& { return this->data; }
