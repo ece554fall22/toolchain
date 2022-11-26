c528fd401bbfbd204b01ff3aa487a69ab5417841
Allison Nicole Seigler <aseigler@tux-139.cae.wisc.edu>
2022-11-26 14:14:07 -0600

    added rest of vector ops and matrix ops

--- libmorph/morph/encoder.h
+++ libmorph/morph/encoder.h
@@ -9,7 +9,9 @@
 namespace isa {
 enum class ScalarArithmeticOp { Add, Sub, Mult, And, Or, Xor, Shr, Shl, Not };
 enum class FloatArithmeticOp { Fadd, Fsub, Fmult, Fdiv};
-enum class VectorArithmeticOp {Vadd, Vsub, Vmult, Vdiv, Vdot, Vdota, Vsadd, Vsmult, Vssub, Vsdiv, Vsma};
+enum class VectorArithmeticOp { Vadd, Vsub, Vmult, Vdiv, Vdot, Vdota, Vindx, Vreduce,
+    Vsplat, Vswizzle, Vsadd, Vsmult, Vssub, Vsdiv, Vsma, Vmax, Vmin, Vcompsel};
+enum class matrixMultiplyOp {writeA, writeB, writeC, matmul, readC, systolicstep};
 }; // namespace isa
 
 class Emitter {
@@ -20,13 +22,15 @@ class Emitter {
     void jumpRegRel(reg_idx rA, s<20> imm, bool link);
 
     void scalarArithmeticImmediate(isa::ScalarArithmeticOp op, reg_idx rD,
-                                   reg_idx rA, s<15> imm);
+                          reg_idx rA, s<15> imm);
     void scalarArithmetic(isa::ScalarArithmeticOp op, reg_idx rD, reg_idx rA,
                           reg_idx rB);
     void floatArithmetic(isa::FloatArithmeticOp op, reg_idx rD, reg_idx rA,
                           reg_idx rB);
     void vectorArithmetic(isa::VectorArithmeticOp op, vreg_idx vD, vreg_idx vA,
-                                 vreg_idx vB, reg_idx rD, reg_idx rA, u<4> mask)
+                          vreg_idx vB, reg_idx rD, reg_idx rA, u<4> mask);
+    void matrixMultiply(isa::matmulOp op, vreg_idx vD, vreg_idx vA, vreg_idx vB,
+                          u<3> idx, bool high) 
                                  
 
     auto getData() -> const auto& { return this->data; }
