84d5b4ab232f584f0eb72881abad8f97ebd60a62
erin petra sofiya moon <erin@hecke.rs>
2022-11-28 15:38:57 -0600

    libmorph: improve unimplemented, todo, panic: reference file and line

--- libmorph/morph/util.h
+++ libmorph/morph/util.h
@@ -5,25 +5,25 @@
 /// --- "we have Rust at home"
 class Unimplemented : public std::logic_error {
   public:
-    Unimplemented() : std::logic_error("unimplemented!"){};
+    Unimplemented(const char* file, size_t line) : std::logic_error(fmt::format("unimplemented! at {}:{}", file, line)) {};
+    Unimplemented(const char* file, size_t line, const std::string& desc) : std::logic_error(fmt::format("unimplemented! at {}:{}: {}", file, line, desc)) {};
 };
-inline void unimplemented() { throw Unimplemented(); }
 
 class Todo : public std::logic_error {
   public:
-    Todo() : std::logic_error("todo!"){};
-    Todo(const std::string& desc) : std::logic_error("todo!: " + desc){};
+    Todo(const char* file, size_t line) : std::logic_error(fmt::format("todo! at {}:{}", file, line)) {};
+    Todo(const char* file, size_t line, const std::string& desc) : std::logic_error(fmt::format("todo! at {}:{}: {}", file, line, desc)) {};
 };
-inline void todo() { throw Todo(); }
-inline void todo(const std::string& desc) { throw Todo(desc); }
 
 class Panic : public std::logic_error {
   public:
-    Panic() : std::logic_error("panic!"){};
-    Panic(const std::string& desc) : std::logic_error("panic!: {}" + desc){};
+    Panic(const char* file, size_t line) : std::logic_error(fmt::format("panic! at {}:{}", file, line)) {};
+    Panic(const char* file, size_t line, const std::string& desc) : std::logic_error(fmt::format("panic! at {}:{}: {}", file, line, desc)) {};
 };
-inline void panic() { throw Panic(); }
-inline void panic(const std::string& desc) { throw Panic(desc); }
+
+#define panic(...) throw Panic(__FILE__, __LINE__, ##__VA_ARGS__)
+#define todo(...) throw Panic(__FILE__, __LINE__, ##__VA_ARGS__)
+#define unimplemented(...) throw Panic(__FILE__, __LINE__, ##__VA_ARGS__)
 
 /// --- overload helpers for std::visit
 template <class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
