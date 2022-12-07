f0eecc5c8c065c926a54122050904c55ce6e21d3
erin petra sofiya moon <erin@hecke.rs>
2022-12-06 18:19:03 -0600

    add morpheus assembly sublime-syntax

--- asm/test/smoketest/vector.s
+++ asm/test/smoketest/vector.s
@@ -1,10 +1,11 @@
 vadd 0b1111, v3, v2, v1
 vsub 0b1111, v4, v3, v1
 
-;vsadd 0b1111, v5, v4, r0
+vsadd 0b1111, v5, v4, r0
 
 vidx r1, v4, 3
 vsplat 0b1111, v10, r1
 
 ; v5 <- v6.yxwz
 vswizzle 0b1111, v5, v6, 1, 0, 3, 2
+
