56ef0d9c41a9e285da43c90e5cd1032397abe0ff
garciacalder <garciacalder@ad.engr.wisc.edu>
2022-12-13 00:47:04 -0600

    first version of rand_vector.py

--- testgen/rand_vector.py
+++ testgen/rand_vector.py
@@ -29,30 +29,32 @@ def cli(instr_count, constrain_regs, seed):
 
 # vldi, vsti, vldr, vstr, Vadd, Vsub, Vmult, Vdiv, Vdot, Vdota, Vindx, Vreduce,
 # Vsplat, Vswizzle, Vsadd, Vsmult, Vssub, Vsdiv, vsma, Vmax, Vmin
-    ops = ('vldi', 'vsti', 'vldr', 'vstr', 'Vadd', 'Vsub', 'Vmult', 'Vdiv', 'Vdot', 'Vdota', 'Vindx', 'Vreduce')
+    ops = ('Vadd', 'Vsub', 'Vmult', 'Vdiv')
     rng = Random(seed)
+
 
     # seed in random values for all registers
     print(';; preseed values for registers')
     for r in ALLREGS:
-        lo = rands(rng, 32)
-        # hi = rands(rng, 18)
-        print(f'vldi r{r}, {lo:#x}')
-        # print(f'lih r{r}, {hi:#x}')
+        lo = rands(rng, 18)
+        hi = rands(rng, 18)
+        print(f'lil r{r}, {lo:#x}')
+        print(f'lih r{r}, {hi:#x}')
+
+    # seed in random values for all registers
+    print(';; preseed values for registers')
+    for r in ALLREGS:
+        print(f'vsplat v{r}, r{r}, 0xF')
 
     # then generate `instr_count` random ops
     print(';; random scalar arithmetic')
     for _ in range(instr_count):
         op = rng.choice(ops)
-        if op == 'not':
-            vD = rng.choice(regset)
-            vA = rng.choice(regset)
-            print(f'{op:4} v{vD}, v{vA}')
-        else:
             vD = rng.choice(regset)
             vA = rng.choice(regset)
             vB = rng.choice(regset)
-            print(f'{op:4} v{vD}, v{vA}, v{vB}')
+            imm = rands(rng, 4)
+            print(f'{op:4} v{vD}, v{vA}, {imm:#x}')
 
 if __name__ == '__main__':
     cli()
